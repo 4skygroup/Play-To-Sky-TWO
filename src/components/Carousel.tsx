64b57a48f2094b0711d7224618ca756a6293ac3f
Joel-Henri-NGOUBE <ebosqua.edimo@gmail.com>
2026-07-14 11:13:18 +0200

    Descriptions added

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-// import { useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 type Slide = {
   src: string;
@@ -86,7 +86,7 @@ export default function Carousel() {
     slides[(start + 1) % slides.length],
     slides[(start + 2) % slides.length],
   ];
-  //   const { t } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <div className="min-h-1/2 bg-black flex items-center justify-center gap-6 md:gap-10 px-6 md:px-16 py-10 relative">
@@ -100,36 +100,38 @@ export default function Carousel() {
 
       <div className="flex gap-3 md:gap-6 flex-1 justify-center">
         {three.map((slide, i) => (
-          //   <div
-          //     className="w-fit md:w-[min(280px,28vw)] h-fit"
-          //     key={(start + i) % slides.length}
-          //   >
+          //   <>
           <div
-            className={`group relative aspect-square flex items-center justify-center shrink-0 w-[60vw] md:w-[min(280px,28vw)] ${i > 0 ? "hidden md:flex" : ""}`}
-            style={{
-              backgroundColor: slide.bg,
-              border: slide.border ? `2px solid ${slide.border}` : "none",
-            }}
+            className={`w-[60vw] md:w-[min(280px,28vw)] flex-col ${i > 0 ? "hidden md:flex" : ""}`}
+            key={(start + i) % slides.length}
           >
-            <a
-              href={slide.href}
-              className="relative w-full h-full flex items-center justify-center"
+            <div
+              className={`group relative aspect-square flex items-center justify-center shrink-0 w-[60vw] md:w-[min(280px,28vw)] ${i > 0 ? "hidden md:flex" : ""}`}
+              style={{
+                backgroundColor: slide.bg,
+                border: slide.border ? `2px solid ${slide.border}` : "none",
+              }}
             >
-              <img
-                src={slide.src}
-                alt={`Slide ${((start + i) % slides.length) + 1}`}
-                className="w-full h-full object-contain transition-opacity duration-300 group-hover:opacity-50"
-              />
-              <span className="absolute top-2 right-2 text-white text-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                ↗
-              </span>
-              {/* Hey */}
-            </a>
+              <a
+                href={slide.href}
+                className="relative w-full h-full flex items-center justify-center"
+              >
+                <img
+                  src={slide.src}
+                  alt={`Slide ${((start + i) % slides.length) + 1}`}
+                  className="w-full h-full object-contain transition-opacity duration-300 group-hover:opacity-50"
+                />
+                <span className="absolute top-2 right-2 text-white text-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  ↗
+                </span>
+                {/* Hey */}
+              </a>
+            </div>
+            <p className="text-white text-center px-4 pt-4 max-w-[60vw] md:w-[min(280px,28vw)">
+              {t(slide.description)}
+            </p>
           </div>
-          // <p className="text-white text-center pt-4 px-4 max-w-[60vw] md:max-w-[min(280px,28vw)]">
-          //   {t(slide.description)}
-          // </p>
-          //   </div>
+          //   </>
         ))}
       </div>
 
